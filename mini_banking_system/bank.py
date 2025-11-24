16f6fdb3add71232cf3f36da83b09788e83e72e4
fikayo salu <salufikayo@gmail.com>
2025-11-24 15:56:07 +0100

    Understanding callback hell

--- mini_banking_system/bank.py
+++ mini_banking_system/bank.py
@@ -2,6 +2,7 @@
 """"""
 
 from account import Account
+import os
 
 
 class Bank:
@@ -18,21 +19,28 @@ class Bank:
             if account_number == item.account_number:
                 return item
             else:
-                return False
+                return None
 
     def deposit(self, account_number, amount):
         userAccount = self.find_account(account_number)
         if userAccount:
-            userAccount.deposit(amount)
+            result = userAccount.deposit(amount)
+            return result
+        return None
 
     def withdraw(self, account_number, amount):
-        pass
+        userAccount = self.find_account(account_number)
+        if userAccount:
+            result = userAccount.withdraw(amount)
+            return result
+        return None
 
     def save_data(self):
         pass
 
     def load_data(self):
-        pass
+        if os.path.exists(self.__data_file):
+            with open(self.__data_file, )
 
     def generate_statement(self):
         pass
