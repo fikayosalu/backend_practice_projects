345be2a322112f379647c7bd80f5b728245d0c30
fikayo salu <salufikayo@gmail.com>
2025-12-03 20:26:34 +0100

    feat(app.ts): Practiced HTTP verbs using express REST APIs

--- intro-to-expressjs/natours/app.ts
+++ intro-to-expressjs/natours/app.ts
@@ -5,9 +5,24 @@ import * as fs from "fs";
 const app = express();
 const PORT = 3000;
 
+type tourFrame = {
+	id: number;
+	name: string;
+	duration: number;
+	maxGroupSize: number;
+	difficulty: string;
+	ratingsAverage: number;
+	ratingsQuantity: number;
+	price: number;
+	summary: string;
+	imageCover: string;
+	images: string[];
+	startDates: string[];
+};
+
 app.use(express.json());
 
-const tours = JSON.parse(
+const tours: tourFrame[] = JSON.parse(
 	fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, "utf-8")
 );
 
@@ -30,9 +45,32 @@ app.get("/api/v1/tours", (req: Request, res: Response) => {
 	});
 });
 
+app.get("/api/v1/tours/:id", (req: Request, res: Response) => {
+	console.log(req.params);
+	const id = Number(req.params.id);
+	console.log(id);
+
+	const tour = tours.find((obj) => {
+		return obj.id === id;
+	});
+	if (tour) {
+		res.status(200).json({
+			status: "success",
+			data: {
+				tour,
+			},
+		});
+	} else {
+		res.status(404).json({
+			status: "failed",
+			message: "Invalid ID",
+		});
+	}
+});
+
 app.post("/api/v1/tours", (req: Request, res: Response) => {
 	// console.log(req.body);
-	const newId = tours[tours.length - 1].id + 1;
+	const newId = tours[tours.length - 1]!.id + 1;
 	const newTour = { id: newId, ...req.body };
 	tours.push(newTour);
 	fs.writeFile(
@@ -47,6 +85,37 @@ app.post("/api/v1/tours", (req: Request, res: Response) => {
 			});
 		}
 	);
+});
+app.patch("/api/v1/tours/:id", (req: Request, res: Response) => {
+	const id = Number(req.params.id);
+	if (id > tours.length) {
+		return res.status(404).json({
+			status: "failed",
+			message: "Invalid ID",
+		});
+	}
+	return res.status(200).json({
+		status: "success",
+		data: {
+			tour: "<Updated tour here>",
+		},
+	});
+});
+
+app.delete("/api/v1/tours/:id", (req: Request, res: Response) => {
+	const id = Number(req.params.id);
+	if (id > tours.length) {
+		return res.status(404).json({
+			status: "failed",
+			message: "Invalid ID",
+		});
+	}
+	return res.status(204).json({
+		status: "success",
+		data: {
+			tour: null,
+		},
+	});
 });
 
 app.listen(PORT, () => {
