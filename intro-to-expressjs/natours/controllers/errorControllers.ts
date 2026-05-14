557dede43150c52a40f37fd99dfbd6ad1605f3d3
fikayo salu <salufikayo@gmail.com>
2026-05-14 02:38:15 +0100

    feat: add error handlers for mongodb errors

--- intro-to-expressjs/natours/controllers/errorControllers.ts
+++ intro-to-expressjs/natours/controllers/errorControllers.ts
@@ -1,6 +1,24 @@
 import { Request, Response, NextFunction } from "express";
 import AppError from "../utils/appError";
 
+interface MongooseErr {
+  path: string;
+  value: string;
+  keyValue: { [key: string]: string };
+}
+
+const handleCastErrDB = (err: MongooseErr) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateErrorDB = (err: MongooseErr) => {
+  const value = err.keyValue.name;
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err: AppError, res: Response) => {
   return res.status(err.statusCode).json({
     status: err.status,
@@ -32,7 +50,7 @@ const sendErrorProd = (err: AppError, res: Response) => {
 };
 
 const globalErrorHandler = (
-  err: AppError,
+  err: any,
   req: Request,
   res: Response,
   next: NextFunction,
@@ -40,8 +58,22 @@ const globalErrorHandler = (
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
   if (process.env.NODE_ENV === "development") {
-    sendErrorDev(err, res);
+    let error = {
+      ...err,
+      message: err.message,
+      name: err.name,
+      stack: err.stack,
+    };
+
+    if (error.name === "CastError") error = handleCastErrDB(error);
+    if (error.code === 11000) error = handleDuplicateErrorDB(error);
+    sendErrorDev(error, res);
   } else if (process.env.NODE_ENV === "production") {
+    let error = { ...err };
+
+    if (error.name === "CastError") error = handleCastErrDB(error);
+
+    if (error.code === 11000) error = handleDuplicateErrorDB(error);
     sendErrorProd(err, res);
   }
 };
