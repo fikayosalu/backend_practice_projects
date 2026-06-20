8e2cdc41e61a3728ace11dba6e335a863d1b8168
fikayo salu <salufikayo@gmail.com>
2026-06-20 22:32:28 +0100

    feat: add function comments

--- contact_form_fastapi/main.py
+++ contact_form_fastapi/main.py
@@ -1,16 +1,21 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 
+# Initialize FastApi
 app = FastAPI()
 
 
 class ContactMessage(BaseModel):
+    """This class creates a model for the
+    request body and is validated by fastapi"""
+
     name: str
     email: str
-    message: str = Field(min_length=10, description="Length must be greater than 9")
+    message: str = Field(min_length=10)
     subject: str = "General Inquiry"
 
 
 @app.post("/contact")
 def contact(contact_message: ContactMessage):
+    """Accepts a request body and returns the message info"""
     return {"status": "success", "info": f"{contact_message.message}"}
