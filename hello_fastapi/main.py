020e7c85b60fd178fb58dfa27ed7f6f94b8178cd
fikayo salu <salufikayo@gmail.com>
2026-06-20 20:24:49 +0100

    feat(main): set up persoanlized greeting endpoint

--- hello_fastapi/main.py
+++ hello_fastapi/main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"status": "success", "message": "Welcome to FastApi"}
+
+
+@app.get("/greet/{name}")
+def greet(name: str):
+    return {"status": "success", "message": f"Hi there {name}, welcome🙂"}
