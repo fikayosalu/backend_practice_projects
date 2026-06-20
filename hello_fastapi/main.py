79758b8f899c1e46f713fe3297082dc7a0d2068c
fikayo salu <salufikayo@gmail.com>
2026-06-20 20:50:56 +0100

    feat: add endpoint that accepts query parameter

--- hello_fastapi/main.py
+++ hello_fastapi/main.py
@@ -1,8 +1,15 @@
+import random
 from datetime import datetime
 
 from fastapi import FastAPI
 
 app = FastAPI()
+
+quotes = [
+    "One day hungry man go chop",
+    "Rain wey beat me today go wash my Benz tomorrow",
+    "It gets a little easier everyday, but you have to do it everyday",
+]
 
 
 @app.get("/")
@@ -18,3 +25,11 @@ def greet(name: str):
 @app.get("/time")
 def get_time():
     return datetime.now()
+
+
+@app.get("/quote")
+def get_quote(index: int = None):
+    quote = random.choice(quotes)
+    if index is None or index > 2:
+        return {"status": "success", "message": quote}
+    return {"status": "success", "message": quotes[index]}
